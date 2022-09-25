03f6432b3226dcee51561a06bed1e272700dd4ef
Man07har <thakurmanoharkumar7@gmail.com>
2022-09-25 23:59:43 +0530

    just the inital push

---
+++ src/components/Navbar.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import { ReactDOM } from 'react-dom/client';
+const Navbar = () => {
+  return (
+    <div>
+        <span className="bg-purple-900 text-5xl underline hover:pointer-cursor">Navbar</span>
+    </div>
+  )
+}
+
+export default Navbar;
