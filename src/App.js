fa9753f4eba6e5525c69ef71b23d49e6b02b46fd
Man07har <thakurmanoharkumar7@gmail.com>
2022-09-26 22:20:57 +0530

    created navigation bar and footer

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import React from 'react'
 import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from './components/Home';
+import Blogs from './components/About';
+import About from './components/Blogs';
+import Hello from './components/Hello';
+import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div>
-<Navbar />
-<h1>this is me</h1>
+    <div className='box-border'>
+  <Router>
+    <Navbar />
+    <Routes>
+      <Route exact path='src\components\Home.js' element={<Home />} />
+      <Route exact path='src\components\About.js' element={<About />} />
+      <Route exact path='src\components\Blogs.js' element={<Blogs />} />
+    </Routes>
+  </Router>
+  <Hello />
+  <Footer />
     </div>
   )
 }
---
+++ src/components/Hello.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div className='h-[1000px]'>Hello</div>
+  )
+}
+
+export default Hello
