e3baba31d01d4f84de87108bcbbf3ee2c5f8cd51
Man07har <thakurmanoharkumar7@gmail.com>
2022-09-27 00:28:36 +0530

    added about section and done some changes in footer and navbar and app

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,87 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div className='bg-red-800'>About</div>
-  )
+    <div className='bg-gray-800 '>
+     <div className='div_left  '>
+      <img src="https://cryptohustle.com/wp-content/uploads/2016/02/web-Main.jpg" alt="bitcoin" className=' h-[686px] w-[1640px]' />
+      </div>
+      <div className='div_right'>
+      <h1 className='text-white mx-4 my-4'>A cryptocurrency, crypto-currency, or crypto is a digital currency
+       designed to work as a medium of exchange through a computer network that
+        is not reliant on any central authority, such as a government or bank, to
+         uphold or maintain it. It is a decentralized system for verifying
+          that the parties to a transaction have the money they claim to have, 
+          eliminating the need for traditional intermediaries, such as banks, when 
+          funds are being transferred between two entities. Individual coin ownership records are stored in a digital ledger, which is a computerized database using strong cryptography to secure transaction records, to control the creation of additional coins, and to verify the transfer of coin ownership.[4][5][6] Despite their name, cryptocurrencies are not considered to be currencies in the traditional sense and while varying treatments have been applied to them, including classification as commodities, securities, as well as currencies, cryptocurrencies are generally viewed as a distinct asset class in practice.[7][8][9] Some crypto schemes use validators to maintain the cryptocurrency. In a proof-of-stake model, owners put up their tokens as collateral. In return, they get authority over the token in proportion to the amount they stake. Generally, these token stakers get additional ownership in the token over time via network fees, newly minted tokens or other such reward mechanisms.[10]
+
+Cryptocurrency does not exist in physical form (like paper money) and is typically not issued by a central authority. Cryptocurrencies typically use decentralized control as opposed to a central bank digital currency (CBDC).[11] When a cryptocurrency is minted or created prior to issuance or issued by a single issuer, it is generally considered centralized. When implemented with decentralized control, each cryptocurrency works through distributed ledger technology, typically a blockchain, that serves as a public financial transaction database.[12] Traditional asset classes like currencies, commodities, and stocks, as well as macroeconomic factors, have modest exposures to cryptocurrency returns.[13]
+
+The first decentralized cryptocurrency was Bitcoin, which first released as open-source software in 2009. As of March 2022 there were more than 9,000 other cryptocurrencies in the marketplace, of which more than 70 had a market capitalization exceeding $1 billion.</h1>
+      </div>
+      <div>
+        <div class='1  my-2 mx-[300px]'>
+          <div class='1.1 flex'>
+            <div class='img'>
+<img src="#" alt="coin_image" />
+            </div>
+            <div class='text'>
+            <h1>Bitcoin</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+      <div class='2  my-2 mx-[300px]'>
+          <div class='2.1 flex'>
+            <div class='img'>
+            <img src="#" alt="coin_image" />
+            </div>
+            <div class='text'>
+            <h1>Ethereum</h1>
+            </div>
+          </div>
+        </div>
+        <div class='3 my-2 mx-[300px] '>
+          <div class='3.1 flex'>
+            <div class='img'>
+<img src="#" alt="coin_image" />
+            </div>
+            <div class='text'>
+            <h1>Tether</h1>
+            </div>
+          </div>
+        </div>
+        <div class='4 my-2 mx-[300px] '>
+          <div class='4.1 flex'>
+            <div class='img'>
+<img src="#" alt="coin_image" />
+            </div>
+            <div class='text'>
+                <h1>USD Coin</h1>
+            </div>
+          </div>
+        </div>
+        <div class='5 my-2 mx-[300px]'>
+          <div class='5.1 flex'>
+            <div class='img'>
+<img src="#" alt="coin_image" />
+            </div>
+            <div class='text'>
+            <h1>BNB</h1>
+            </div>
+          </div>
+        </div>
+        <div class='6 my-2 mx-[300px]'>
+          <div class='6.1 flex'>
+            <div class='img'>
+<img src="#" alt="coin_image" />
+            </div>
+            <div class='text'>
+          <h1>XRP</h1>
+            </div>
+          </div>
+        </div>
+      </div>
+   )
 }
 
 export default About
